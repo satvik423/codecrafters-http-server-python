6b7035378c56ea040289be3f4dce3a6cf15cb8a3
Satvik R Shetty <satvikrshetty423@gmail.com>
2024-07-20 19:56:16 +0530

    Respond with 200

--- app/main.py
+++ app/main.py
@@ -9,8 +9,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    client_socket, address = server_socket.accept() # wait for client
+    print(f"Connection from {address} has been established.")
+    
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.sendall(response.encode("utf-8"))
+    client_socket.close()
 
 if __name__ == "__main__":
     main()
